ca2ec150f89c4a105c764fb76cd78f0b66a67e24
MaiNagata <92007523+MaiNagata@users.noreply.github.com>
2021-10-06 23:17:25 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -4,13 +4,20 @@ function setup() {
 
 function draw() {
   background(0);
+  stroke(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(123, 17, 130);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
   fill(255);
+  triangle(0,108,106,108,106,0);
+  fill(241,159,182);
+  strokeWeight(0);
   textSize(32);
   textFont("serif");
   text("46", 68, 100);
+  strokeWeight(1);
+  stroke(241,159,182);
+  line(104,106,104,4);
+  line(104,106,4,106);
+  line(104,4,4,106);
 }
