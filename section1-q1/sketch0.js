5562825f258a8b4f06f16559b5318fadf780208e
MaiNagata <92007523+MaiNagata@users.noreply.github.com>
2021-10-13 20:28:53 +0900

    change section1-q1_2

---
+++ section1-q1/sketch0.js
@@ -0,0 +1,21 @@
+// 練習問題：神戸市のマーク
+function setup(){
+  createCanvas(200, 200);
+  background(255);
+  strokeWeight(0);
+  fill(14, 47, 146);
+  arc(100, 100, 100, 100, 0, TWO_PI);
+  fill(255);
+  arc(95, 95, 90, 90, 0, TWO_PI);
+  fill(22, 131, 46);
+  arc(93, 93, 85, 85, 0, TWO_PI);
+  fill(255);
+  arc(93, 98, 78, 78, 0, TWO_PI);
+  fill(77);
+  textFont("serif");
+  textSize(20);
+  text('KOBE', 65, 90);
+  textSize(8);
+  text('UNIVERSITY', 75, 105);
+
+}
