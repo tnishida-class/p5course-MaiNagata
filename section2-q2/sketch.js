5cbb17f18de560ee228146dbbbeb06370c27ecbc
MaiNagata <92007523+MaiNagata@users.noreply.github.com>
2021-10-17 22:26:03 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,20 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    if((i+j)%2==1){
+      fill(150);
     }
+    else{
+      fill(255);
+    }
+    rect(size*i,size*j,size,size);
+    if(j<3 && (i+j)%2==1){
+      fill(255,0,0);
+    }
+    else if(j>4 && (i+j)%2==1){
+      fill(0);
+    }
+    ellipse(size/2*(2*(i+1)-1),size/2*(2*(j+1)-1),22);
+  }
   }
 }
