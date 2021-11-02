1b5ce43101d0c1d19a9eeb66baec1edff9e06f98
MaiNagata <92007523+MaiNagata@users.noreply.github.com>
2021-11-02 12:01:52 +0900

    change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -20,5 +20,10 @@ function setup(){
   let px, py; // 線を引くために一つ前の点を覚えておく変数
   for(let i = 0; i < scores.length; i++){
     // BLANK[1]
+    const h = height - height * scores[i] / 100;
+    ellipse(i * dx, h, 5);
+    px = (i-1) * dx;
+    py = height - height * scores[i-1] / 100
+    line(px, py, i * dx, h);
   }
 }
